afe9bc3b07f7573ffa0d7dc1a40d598a59b05eb6
Moii L <96174525+MoiiL@users.noreply.github.com>
2022-05-07 02:26:29 +0530

    Create GRPAS1.py

---
+++ GRPAS/W1-Python-Recap/GRPAS1.py
@@ -0,0 +1,27 @@
+#Finding the minimum difference value in a list
+
+def find_Min_Difference(L,P):
+    for i in range(len(L)):
+        for j in range(i+1,len(L)):
+            if L[i]>L[j]:
+               L[i],L[j]=L[j],L[i]
+    start = 0
+    min_diff = max(L)
+    while len(L)-start >= P:
+        temp_ls = L[start:start + P]
+        diff = temp_ls[-1] - temp_ls[0]
+        if start == 0:
+            min_diff = diff
+        if diff < min_diff:
+            min_diff = diff
+        start += 1
+    return min_diff
+        
+            
+            
+    
+
+
+L=eval(input().strip())
+P=int(input())
+print(find_Min_Difference(L,P))
