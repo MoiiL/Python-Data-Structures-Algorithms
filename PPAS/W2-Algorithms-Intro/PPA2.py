8e12033c1d47a2eb88e2f75f3d755a1909f56026
Vader L <96174525+MoiiL@users.noreply.github.com>
2022-05-15 04:49:47 +0530

    Update PPA2.py

--- PPAS/W2-Algorithms-Intro/PPA2.py
+++ PPAS/W2-Algorithms-Intro/PPA2.py
@@ -1,2 +1,19 @@
+
+Method 1:
 def sortInRange(ls,r):
   return ls.sort()
+
+Method 2:
+def sortInRange(L,r):
+    d = {}
+    for i in L:
+        try:
+            d[i]+=1
+        except KeyError:
+            d[i]=1
+    
+    L.clear()
+    for i in range(r):
+        L.extend([i]*d[i])
+        
+    return L
