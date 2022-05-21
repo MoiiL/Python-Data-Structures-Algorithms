ed3ecca6f24e73e04c43b84e82a4fce06978f4d5
Anakin Skywalker <96174525+MoiiL@users.noreply.github.com>
2022-05-22 00:17:11 +0530

    Update GRPA2.py

--- GRPAS/W3-Sorting-Stacks-Queues/GRPA2.py
+++ GRPAS/W3-Sorting-Stacks-Queues/GRPA2.py
@@ -1,16 +1,33 @@
-def DishPrepareOrder(ls):
-    temp = set(ls)
-    templs = []
-    for t in temp:
-        templs.append((t,ls.count(t)))
-    return(display(templs))
-        
-def display(ls):
-    ls = sorted(ls,key = lambda x : x[1], reverse = True)
-    temp = []
-    for l in ls:
-        temp.append(l[0])
+def check_operand(s,n1,n2):
+    temp = 0
+    if s == '+':
+        temp = n2 + n1
+    elif s == '-':
+        temp = n2 - n1
+    elif s == '*':
+        temp = n2 * n1
+    elif s == '/':
+        temp = n2/n1
+    elif s == '**':
+        temp = n2**n1
     return temp
-    
-nums = eval(input())
-print(DishPrepareOrder(nums))
+
+
+def EvaluateExpression(ls):
+    ls = ls.split()
+    num , i = len(ls) , 0
+    temp = []
+    while num > 0:
+        n = ls[i]
+        try:
+            temp.append(int(n))
+        except:
+            n1 = temp.pop()
+            n2 = temp.pop()
+            t = check_operand(ls[i], n1, n2)
+            temp.append(t)
+        num -= 1
+        i += 1
+    return str(temp[0])
+
+print(float(EvaluateExpression(input())))
