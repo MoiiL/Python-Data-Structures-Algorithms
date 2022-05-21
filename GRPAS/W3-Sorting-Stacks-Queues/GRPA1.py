d24e69651fc2a6463a86f1510919f56b96f2db9b
Anakin Skywalker <96174525+MoiiL@users.noreply.github.com>
2022-05-22 00:16:36 +0530

    Update GRPA1.py

--- GRPAS/W3-Sorting-Stacks-Queues/GRPA1.py
+++ GRPAS/W3-Sorting-Stacks-Queues/GRPA1.py
@@ -1,1 +1,17 @@
-
+def DishPrepareOrder(ls):
+    temp = set(ls)
+    templs = []
+    for t in temp:
+        templs.append((t,ls.count(t)))
+    return(sortlist(templs))
+        
+def sortlist(ls):
+    ls = sorted(ls,key = lambda x : x[1], reverse = True)
+    temp = []
+    for l in ls:
+        temp.append(l[0])
+    return temp
+    
+    
+nums = eval(input())
+print(DishPrepareOrder(nums))
